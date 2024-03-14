d4ea2f724cab02ae1edb3b9bc6b68302c489ea30
taccumei <dangngoctram1903@gmail.com>
2024-03-14 14:36:02 -0700

    Add Origins, cook time, price of foods

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
+import { getAll } from '../../services/foodService';
+import Thumbnails from '../../components/Thumbnails/Thumbnails';
+
+const initialState = { foods: [] };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FOODS_LOADED': 
+      return { ...state, foods: action.payload }
+    default:
+      return state;
+  }
+}
+
 
 export default function HomePage() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { foods } = state;
+
+  useEffect(() => {
+    getAll().then(foods => dispatch({ type: 'FOODS_LOADED', payload: foods }));
+  }, []);
+
   return (
     <div>
-      
+      <Thumbnails foods={foods}/>
     </div>
   )
 }
