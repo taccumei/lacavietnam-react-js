a6afb61e6aff1beeb99974e09768e951845b496e
taccumei <dangngoctram1903@gmail.com>
2024-03-18 15:12:59 -0700

    clear text in search box when go back to home page from notfound page

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import classes from './search.module.css';
 
 export default function Search() {
   const [term, setTerm] = useState('');
   const navigate = useNavigate();
-  const { searchTerm } = useParams;
-
+  const { searchTerm } = useParams();
+  
   const search = async () => {
     term ? navigate('/search/' + term) : navigate('/');
   };
+
+    useEffect(() => {
+    setTerm(searchTerm ?? '');
+    }, [searchTerm]);
 
   return (
     <div className={classes.container}>
@@ -18,7 +22,7 @@ export default function Search() {
         placeholder='Craving for...'
         onChange={e => setTerm(e.target.value)}
         onKeyUp={e => e.key === 'Enter' && search()}
-        defaultValue={searchTerm}
+        // defaultValue={searchTerm}
         value={term}
       />
       <button onClick={search}>Search</button>
