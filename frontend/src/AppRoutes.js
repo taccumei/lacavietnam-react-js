e3d0925a9f06968171f375415625e640397517f1
taccumei <dangngoctram1903@gmail.com>
2024-03-17 20:25:57 -0700

    Create CartPage componet and add to route

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import HomePage from './pages/Home/HomePage'
 import { Route, Routes } from 'react-router-dom'
 import FoodPage from './components/FoodPage/FoodPage'
+import CartPage from './components/CartPage/CartPage'
 
 export default function AppRoutes() {
   return (
@@ -10,7 +11,8 @@ export default function AppRoutes() {
         <Route path="/" element={<HomePage />}/>
         <Route path="/search/:searchTerm" element={<HomePage />}/>
         <Route path="/tag/:tag" element={<HomePage />}/>
-        <Route path="/food/:id" element={<FoodPage />}/>
+        <Route path="/food/:id" element={<FoodPage />} />
+        <Route path="/cart" element={<CartPage/>}/>
       </Routes>
     </div>
   )
