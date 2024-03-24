b323e618046c193506ff0a35a0033f209fdc2f8c
taccumei <dangngoctram1903@gmail.com>
2024-03-24 09:17:49 -0700

    Create Loading interceptor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import AppRoutes from "./AppRoutes";
 import Header from "./components/Header/Header";
+import { useLoading } from "./hooks/useLoading";
+import { setLoadingInterceptor } from './components/Interceptors/LoadingInterceptor.js';
+import Loading from "./components/Loading/Loading";
+import { useEffect } from "react";
+
 
 function App() {
+  const { showLoading, hideLoading } = useLoading();
+  
+  useEffect(() => {
+    setLoadingInterceptor({ showLoading, hideLoading });
+  }, []);
+  
   return (
     <>
+      <Loading/>
       <Header />
       <AppRoutes/>
     </>
