01983b3ff761c8dcd937e8149890ef80afef188d
taccumei <dangngoctram1903@gmail.com>
2024-03-24 11:19:26 -0700

    Create auth middleware

---
+++ backend/src/middleware/auth.mid.js
@@ -0,0 +1,17 @@
+import { verify } from 'jsonwebtoken';
+import { UNAUTHORIZED } from '../constanst/httpStatus';
+
+export default (req, res, next) => {
+  const token = req.headers.access_token;
+
+  if (!token) return res.status.UNAUTHORIZED.send();
+
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+  } catch(error) {
+    res.status(UNAUTHORIZED).send();
+  }
+
+  return next();
+}
---
+++ backend/src/routers/order.router.js
@@ -0,0 +1,4 @@
+import { Router } from 'express';
+import handler from 'express-async-handler';
+
+const router = Router();
